4908c49e9e510426798481dd0fc55dff1dff9484
Freewind <nowindlee@gmail.com>
2021-01-27 01:02:55 +0800

    typescript-extend-string-type-demo

---
+++ global.ts
@@ -0,0 +1,6 @@
+declare global {
+  interface String {
+    padZero(length: number): string;
+  }
+}
+export {}
--- hello.ts
+++ hello.ts
@@ -1,1 +1,11 @@
-console.log('Hello');
+import './global';
+
+String.prototype.padZero = function (length: number) {
+  let d = String(this)
+  while (d.length < length) {
+    d = '0' + d;
+  }
+  return d;
+};
+
+console.log('hi'.padZero(5))
